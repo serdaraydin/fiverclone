6694405ed346bc0667754d54423eec94cf63b7ef
serdaraydin <serdara2000@gmail.com>
2024-06-18 21:41:59 +0300

    auth and database

---
+++ components/auth/loading.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export const Loading = () => {
+  <div className="h-full w-full flex justify-center items-center">
+    <Image
+      src="/logo.svg"
+      alt="loading"
+      width={120}
+      height={120}
+      className="animate-pulse duration-700"
+    />
+  </div>;
+};
---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://communal-cat-91.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
